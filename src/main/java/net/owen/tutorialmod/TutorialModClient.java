23e754f9c4974da9d587fc7882e500ff1d8c121e
omueller5 <hulkmueller@gmail.com>
2023-12-03 19:58:48 -0600

    Version 0.0.1 commit, git repository created

---
+++ src/main/java/net/owen/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.owen.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.http.cookie.ClientCookie;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
