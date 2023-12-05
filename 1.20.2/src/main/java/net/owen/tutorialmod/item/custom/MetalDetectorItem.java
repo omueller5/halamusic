2d791ce6fdbef26ef8a5eaecee6d5260c79b9d42
omueller5 <hulkmueller@gmail.com>
2023-12-04 22:35:25 -0600

    added tags for the metal detector

--- 1.20.2/src/main/java/net/owen/tutorialmod/item/custom/MetalDetectorItem.java
+++ 1.20.2/src/main/java/net/owen/tutorialmod/item/custom/MetalDetectorItem.java
@@ -12,6 +12,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.owen.tutorialmod.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -23,15 +24,15 @@ public class MetalDetectorItem extends Item {
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        if (!context.getWorld().isClient){
+        if(!context.getWorld().isClient()) {
             BlockPos positionClicked = context.getBlockPos();
             PlayerEntity player = context.getPlayer();
             boolean foundBlock = false;
 
-            for(int i = 0; i<= positionClicked.getY() + 64; i++){
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
                 BlockState state = context.getWorld().getBlockState(positionClicked.down(i));
 
-                if (isValuableBlock(state)) {
+                if(isValuableBlock(state)) {
                     outputValuableCoordinates(positionClicked.down(i), player, state.getBlock());
                     foundBlock = true;
 
@@ -39,24 +40,24 @@ public class MetalDetectorItem extends Item {
                 }
             }
 
-            if(!foundBlock){
+            if(!foundBlock) {
                 player.sendMessage(Text.literal("No Valuables Found!"));
             }
-
         }
 
-        context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+        context.getStack().damage(1, context.getPlayer(),
+                playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
 
         return ActionResult.SUCCESS;
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
-        player.sendMessage(Text.literal("Found " + block.asItem().getName().getString() +
-                " at " + "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), false);
+        player.sendMessage(Text.literal("Found " + block.asItem().getName().getString() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), false);
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
     }
 
     @Override
