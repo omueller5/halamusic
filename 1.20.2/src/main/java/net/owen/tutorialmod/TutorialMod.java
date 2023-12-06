04a8dc2ca9ed3dcb86be565eda4314230875bac6
omueller5 <hulkmueller@gmail.com>
2023-12-06 00:41:35 -0600

    modified vanilla loot tables

--- 1.20.2/src/main/java/net/owen/tutorialmod/TutorialMod.java
+++ 1.20.2/src/main/java/net/owen/tutorialmod/TutorialMod.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.owen.tutorialmod.block.ModBlocks;
 import net.owen.tutorialmod.item.ModItemGroups;
 import net.owen.tutorialmod.item.ModItems;
+import net.owen.tutorialmod.util.ModLootTableModifiers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +21,8 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		ModLootTableModifiers.modifyLootTables();
 
 		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
