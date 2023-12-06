473e8b268d06319a38644bfa6726c298ad168870
omueller5 <hulkmueller@gmail.com>
2023-12-06 00:26:44 -0600

    added status effect for ruby armor

--- 1.20.2/src/main/java/net/owen/tutorialmod/item/ModItems.java
+++ 1.20.2/src/main/java/net/owen/tutorialmod/item/ModItems.java
@@ -9,6 +9,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.owen.tutorialmod.TutorialMod;
 import net.owen.tutorialmod.item.custom.MetalDetectorItem;
+import net.owen.tutorialmod.item.custom.ModArmorItem;
 
 public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
@@ -31,7 +32,7 @@ public class ModItems {
             new HoeItem(ModToolMaterial.RUBY, 0, 0f, new FabricItemSettings()));
 
     public static final Item RUBY_HELMET = registerItem("ruby_helmet",
-            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings()));
+            new ModArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings()));
     public static final Item RUBY_CHESTPLATE = registerItem("ruby_chestplate",
             new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
     public static final Item RUBY_LEGGINGS = registerItem("ruby_leggings",
